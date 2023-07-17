cbd201fdd41c397ae3d624f8bc1be39a527fbbd4
Macedo-Larissa <llarissavmacedo@gmail.com>
2023-07-17 14:23:27 -0300

    main

---
+++ bim2/angular/testeangular/src/app/meu-componente/meu-componente.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-componente',
+  templateUrl: './meu-componente.component.html',
+  styleUrls: ['./meu-componente.component.css']
+})
+export class MeuComponenteComponent {
+  nome: string;
+
+  constructor() {
+    this.nome = 'Larissa';
+  }
+}
