1e0c4260781b789a761a9b9bd6698984495afdd5
Macedo-Larissa <llarissavmacedo@gmail.com>
2023-08-30 14:08:45 -0300

    main

--- projeto/src/app/turma/turma.component.ts
+++ projeto/src/app/turma/turma.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-turma',
   templateUrl: './turma.component.html',
   styleUrls: ['./turma.component.css']
 })
-export class TurmaComponent {
 
+export class TurmaComponent implements OnInit {
+  mensagem: string;
+  
+  constructor() {
+    this.mensagem = '';
+  }
+
+  ngOnInit() {}
+
+  alterarMensagem(n:string) {
+    this.mensagem = `Seja bem-vindo, ${n}!`;
+  }
 }
