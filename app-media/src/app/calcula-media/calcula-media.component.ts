832300550cb9960b5e157ccdb522a3a42bae9c43
Macedo-Larissa <llarissavmacedo@gmail.com>
2023-09-06 14:25:07 -0300

    main

--- app-media/src/app/calcula-media/calcula-media.component.ts
+++ app-media/src/app/calcula-media/calcula-media.component.ts
@@ -6,26 +6,38 @@ import { Nota } from './nota (1)';
   templateUrl: './calcula-media.component.html',
   styleUrls: ['./calcula-media.component.css']
 })
+
 export class CalculaMediaComponent {
 
   boletim: Nota;
   resultado: string;
+  mediaParcial: number;
 
   constructor() {
     this.boletim = new Nota(0, 0, 0, 0);
     this.resultado = '';
+    this.mediaParcial = 0;
   }
 
-  calcularMediaParcial(b1: number, b2: number, b3: number, b4: number,) {
+  calcularMediaParcial(b1: number, b2: number, b3: number, b4: number) {
     this.boletim.alterarBim1(b1);
     this.boletim.alterarBim2(b2);
     this.boletim.alterarBim3(b3);
     this.boletim.alterarBim4(b4);
     
     let mp = this.boletim.calcularMédiaParcial();
+    this.mediaParcial = mp;
     let sit = this.boletim.obterSituação();
 
-    this.resultado = `${sit} com média parcial ${mp}.`;
+    this.resultado = `${sit} com média  ${mp}.`;
 
+  }
+
+  calcularMediaFinal(naf: number) {
+
+    let mf = this.boletim.calcularMédiaFinal(naf);
+    let sit2 = this.boletim.obterSituação();
+
+    this.resultado = `${sit2} com média  ${mf};`;
   }
 }
